a1db11b19312ca85d4b450c13f60fc9ffd2e854d
JNelson <nelson.jimmy@gmail.com>
2018-09-30 16:23:29 -0600

    Updates customer.java and application.java to fix typos and change core functionality.

--- src/main/java/hello/Customer.java
+++ src/main/java/hello/Customer.java
@@ -4,7 +4,7 @@ public class Customer {
   private long id;
   private String firstName, lastName;
 
-  public Customer(long id, String firstName, Stirng lastName) {
+  public Customer(long id, String firstName, String lastName) {
     this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
@@ -15,6 +15,5 @@ public class Customer {
     return String.format(
             "Customer[id=%d, firstName='%s', lastName='%s']",
             id, firstName, lastName);
-    )
   }
 }
